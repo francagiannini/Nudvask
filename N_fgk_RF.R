0d0ac57c1e9873591521ecfe01f9508b050e8d6b
Franca Giannini-Kurina <au710823@uni.au.dk>
2023-10-06 15:29:38 +0200

    finally

--- N_fgk_RF.R
+++ N_fgk_RF.R
@@ -13,6 +13,9 @@ library(doParallel)
 library(rpart)
 library(mlbench)
 
+theme_set(theme_bw())
+theme_update(panel.grid = element_blank())
+
 # data ----
 
 df_m_imp <- readRDS("df_m_imp.RDS")
@@ -260,7 +263,7 @@ df_gen_simp_ind <- df_gen |>
   )) |>
   drop_na()
 
-df_gen_simp_ind <- df_gen_simp_ind[complete.cases(df_gen_simp_ind),] |>  
+df_gen_simp_ind <- droplevels(df_gen_simp_ind[complete.cases(df_gen_simp_ind),] )|>  
   ##sample_frac(0.2) |> 
   filter(!Winter_nles5==7)
 
@@ -298,8 +301,93 @@ modellist_sim_ind <- readRDS("rffit_sim_ind.RDS")
 res <- resamples(modellist_sim_ind)
 summary(res)
 
+## simplified independent max reduced----
 
-## prediction and communicating results ----
+df_gen_simp_ind_max <- df_gen |> 
+  select(!c(site_eng, harvest_year,
+            afstro_sum_month,afstro_cumsumhy,afstro_sumhy,
+            "N_min_year.1","N_min_year.2","N_f_year.1","N_f_year.2",
+            "N_org_year","N_org_year.1","N_org_year.2",
+            "prev_Main_nles5","prev_Winter_nles5",
+            "tvspp_month","drain_days",
+            "Precip_sum60","Precip_sum90","Precip_sum180",
+            "AirTemp_ave60","AirTemp_ave90","AirTemp_ave180",
+            "tvspp_60","tvspp_90","season","N_f_year","Globrad_ave_month",
+            "Precip_sumhy", "Precip_sum365","AirTemp_avehy", "Precip_sum_month",
+            "WC","Globrad_avehy"
+            
+            
+  )) |>
+  drop_na()
+
+df_gen_simp_ind_max <- droplevels(df_gen_simp_ind_max[complete.cases(df_gen_simp_ind_max),]) |>  
+  ##sample_frac(0.2) |> 
+  filter(!Winter_nles5==7)
+
+fitControl <- trainControl(
+  ## 10-fold CV
+  method = "repeatedcv",
+  number = 10,
+  ## repeated ten times
+  repeats = 2)
+
+tunegrid <- expand.grid(.mtry = seq(1,6,1))
+
+modellist_sim_ind_max <- list()
+
+for (ntree in c(500, 1000, 3000, 6000)) {
+  rffit_sim_ind_max <- caret::train(
+    log(meancon) ~ .,
+    data = df_gen_simp_ind_max,
+    method = 'rf',
+    #preProc = c("center", "scale"),
+    tuneGrid = tunegrid,
+    metric = 'RMSE',
+    ntree = ntree,
+    trControl = fitControl,
+    verbose = FALSE,
+    trace = FALSE)
+  key <- toString(ntree)
+  modellist_sim_ind_max[[key]] <- rffit_sim_ind_max
+}
+
+saveRDS(modellist_sim_ind_max,"rffit_sim_ind_max.RDS")
+
+modellist_sim_ind_max <- readRDS("rffit_sim_ind_max.RDS")
+
+res <- resamples(modellist_sim_ind_max)
+summary(res)
+
+plot(varImp(modellist_sim_ind_max$'1000'))
+
+modellist_sim_ind_max$'1000'$finalModel$coefs
+
+library(pdp)
+
+variables <-list(colnames(df_gen_simp_ind_max))[[1]]
+variables <- variables[variables != c("meancon","clay_cat")] 
+
+pdp_func <- function(i) {
+  
+#m_c_plot <- 
+  partial(modellist_sim_ind_max$'1000', 
+                       pred.var = c(print(i),"clay_cat"), 
+                       train = sample_frac(df_gen,0.1),
+                       chull = TRUE)
+  
+  }
+
+list_rf_sim_ind_max_partial <- lapply(variables, pdp_func)
+
+
+autoplot(#m_c_plot
+         list_rf_sim_ind_max_partial[6][[1]]
+         , contour = TRUE) +
+  geom_smooth()
+
+
+# Linear model ---- 
+
 
 df_gen$month2 <- df_gen$month^2
 df_gen$month3 <- df_gen$month^3
@@ -318,6 +406,7 @@ linear_translate <- lmer(
     WC+
     N_mineral_spring+Precip_sum365+
     Winter_nles5*afstro_cumsumhy2+
+    AirTemp_ave_month*clay_cat+
     Winter_nles5+clay_cat+N_org_year+Globrad_avehy+ Globrad_ave_month+
     month:clay_cat+month2:clay_cat+month3:clay_cat+(1|harvest_year),
   data = df_gen)
@@ -327,14 +416,15 @@ summary(linear_translate)
 #df_gen$lin_pred <- exp(predict(linear_translate, df_gen))
 
 
-linear_translate_ind<- lmer(
+linear_translate_ind<- lme4::lmer(
   log(meancon)~
     month+month2+month3+
     #afstro_cumsumhy+afstro_cumsumhy2+afstro_cumsumhy3+
     AirTemp_avehy+AirTemp_ave_month+
     WC+
     N_mineral_spring+Precip_sum365+
-    Winter_nles5*afstro_cumsumhy2+
+    #Winter_nles5*afstro_cumsumhy2+
+    AirTemp_ave_month*clay_cat+
     Winter_nles5+clay_cat+N_org_year+Globrad_avehy+ Globrad_ave_month+
     month:clay_cat+month2:clay_cat+month3:clay_cat+(1|harvest_year),
   
@@ -342,83 +432,363 @@ linear_translate_ind<- lmer(
 
 summary(linear_translate_ind)
 
+fixed_effect_coeff <- cbind("coefficient"=fixef(linear_translate_ind),
+                            "covariable"=names(fixef(linear_translate_ind))) |> 
+  as.data.frame()
+
+write.table(fixed_effect_coeff,"fixed_effect_coeff.txt", sep="\t")
+
 #df_gen$lin_pred_simp <- exp(predict(linear_translate_simp, df_gen))
 
+
+
+# GBM opt sim ind -----
+
+param_gbm <-  expand.grid(
+  interaction.depth = seq(10,16,3),#c(10,12,14),
+  n.trees = seq(110000,220000,32000),
+  shrinkage = c(0.1,0.0001),
+  n.minobsinnode = seq(5,20,15)
+)
+
+#detectCores()
+#Timedf = data.frame(time="")
+#stime = data.frame(stime)
+
+
+control <- trainControl(method ="repeatedcv", #"cv",
+                        number = 5,
+                        repeats = 2,
+                        allowParallel = TRUE
+                        #savePredictions = "all"
+)
+
+#remotes::install_github("gbm-developers/gbm")
+
+#gbm_raw <- gbm(meancon ~ ., data =df_gen_simp_ind, distribution="tdist")
+
+
+# gbm()
+
+fitt_gbm_non_gamma_bis <- caret::train(
+  log(meancon) ~ .,
+  data =df_gen_simp_ind,
+  method = "gbm",
+  trControl = control,
+  #verbose = FALSE,
+  distribution = "gaussian",
+  tuneGrid = param_gbm
+  
+)
+
+plot(fitt_gbm_non_gamma_bis)
+
+saveRDS(fitt_gbm_non_gamma,"fitt_gbm_none_gamma.RDS")
+
+fitt_gbm_non_gamma <- readRDS("fitt_gbm_none_gamma.RDS")
+
+fitt_gbm_non_gamma$results[rownames(fitt_gbm_non_gamma$bestTune),]$RMSE/mean(log(df_gen$meancon))*100
+
+sqrt(mean((fitt_gbm_non_gamma$finalModel$fit-log(df_gen$meancon))^2))/mean(log(df_gen$meancon))* 100
+
+plot((fitt_gbm_non_gamma$finalModel$shrinkage))
+
+# xgb gradient ----
+
+param_grid_xgb_max <- expand.grid(
+  nrounds = c(500,750,1000),#seq(100, 1000, 300),          # Number of boosting rounds
+  max_depth = c(5, 10),              # Maximum depth of trees
+  eta = c(0.01, 0.1, 0.3),             # Learning rate
+  gamma = c(0.1, 0.2, 0.3),                           # Regularization parameter
+  colsample_bytree = c(0.5, 0.9), # Fraction of features to use in each tree
+  subsample= c(0.5, 0.9),
+  min_child_weight=c(5,20)
+  )
+
+fitt_xgb_max <- caret::train(
+  log(meancon) ~ .,
+  data = df_gen_simp_ind_max,
+  preProc = c("center", "scale"),
+  method = "xgbTree",
+  trControl = trainControl(method="cv", number=5, returnResamp = "all"),
+  tuneGrid = param_grid_xgb_max,
+  verbose = FALSE
+)
+
+summary(fitt_xgb_max)
+
+plot(fitt_xgb_max)
+
+plot(varImp(fitt_xgb_max))
+
+library(xgboost)
+
+importance <-
+  xgb.importance(model = fitt_xgb_max$finalModel) |> 
+  as.data.frame() |> 
+  mutate(Predictor=sub("^(Main_nles5).*", "\\1", Feature)) |>
+  mutate(Predictor=sub("^(Winter_nles5).*", "\\1", Predictor)) |>
+  mutate(Predictor=sub("^(clay_cat).*", "\\1", Predictor)) |> 
+  group_by(Predictor) |> 
+  summarise(Contribution=sum(Gain)*100) |> 
+  arrange(-Contribution)
+
+importance |> ggplot(aes(x=reorder(Predictor, Contribution), y=Contribution))+
+  geom_col(width=0.3)+coord_flip()+theme_hc()+
+  labs(x='Predictors',
+       y="Contribution (%)")
+
+sample_frac(df_gen_simp_ind_max, 0.1) |> 
+  ggpairs(colors='clay_cat') +theme_clean()
+
+
+saveRDS(fitt_xgb_max, "fitt_xgb_max.RDS")
+
+sqrt(mean((predict(fitt_xgb_max,df_gen)-log(df_gen$meancon))^2))/mean(log(df_gen$meancon))* 100
+
+
+library(pdp)
+
+variables <-c("N_mineral_spring","Main_nles5","Winter_nles5",
+              "AirTemp_ave_month","Precip_cumsumhy")
+
+pdp_func <- function(i) {
+  
+  m_c_plot <- 
+  partial(fitt_xgb_max, 
+          pred.var = c(print(i),"clay_cat"), 
+          train = droplevels(sample_frac(df_gen,0.2)),
+          chull = TRUE)
+  
+}
+
+list_xgb_sim_ind_max_partial <- lapply(variables, pdp_func)
+
+
+pdp_Precip_cumsumhy<- 
+  list_xgb_sim_ind_max_partial[[6]] |> 
+  ggplot(aes(x=Precip_cumsumhy, 
+             y=exp(yhat),
+             col=clay_cat,
+             fill=clay_cat))+
+  #scale_x_continuous(breaks = seq(1,12,1))+
+  #scale_colour_solarized('red')+
+  scale_color_stata()+
+  scale_fill_stata()+
+  #geom_col(position = "dodge")+
+  geom_line()+
+  geom_smooth(alpha=0.1)+
+  theme_hc()
+           
+
+ggarrange(pdp_AirTemp_ave_month,
+          pdp_Precip_cumsumhy,
+          pdp_N_mineral_spring, 
+          pdp_month, 
+         
+          pdp_Main_nles5,
+          pdp_Winter_nles5,
+          
+       ncol=2, nrow=3, #, 
+          common.legend = T,  legend="bottom")
+
+
+
+#Prediction and communicating results ----
+
 df_gen_pred <- cbind(df_gen,
-                "pred_rf_full" = as.data.frame(exp(modellist$'2000'$finalModel$predicted))[,1],
-                "pred_rf_full_ind" =as.data.frame(exp(modellist_ind$'2000'$finalModel$predicted))[,1] ,
-                "pred_rf_sim" = as.data.frame(exp(modellist_sim$'2500'$finalModel$predicted))[,1],
-                "pred_rf_sim_ind" = as.data.frame(exp(modellist_sim_ind$'1500'$finalModel$predicted))[,1],
-                "pred_lin"=exp(predict(linear_translate, df_gen)),
-                "pred_lin_ind"=exp(predict(linear_translate_ind, df_gen)) 
-                ) |> 
-  mutate(leach_obs=meancon*afstro_sum_month/100,
+                     "pred_rf_full" = as.data.frame(exp(modellist$'2000'$finalModel$predicted))[,1],
+                     "pred_rf_full_ind" =as.data.frame(exp(modellist_ind$'2000'$finalModel$predicted))[,1] ,
+                     "pred_rf_sim" = as.data.frame(exp(modellist_sim$'2500'$finalModel$predicted))[,1],
+                     "pred_rf_sim_ind_max" = as.data.frame(exp(modellist_sim_ind_max$'1000'$finalModel$predicted))[,1],
+                     "pred_rf_sim_ind" = as.data.frame(exp(modellist_sim_ind$'1500'$finalModel$predicted))[,1],
+                     
+                     "pred_gbm_sim_ind"= exp(fitt_gbm_non_gamma$finalModel$fit),
+                     "pred_xbt_sim_ind" = exp(predict(fitt_xgb,df_gen)),
+                     "pred_xbt_sim_ind_max" = exp(predict(fitt_xgb_max,df_gen)),
+                     
+                     "pred_lin"=exp(predict(linear_translate, df_gen)),
+                     "pred_lin_ind"=exp(predict(linear_translate_ind, df_gen)) 
+) |> 
+  mutate(
+    leach_a_obs=meancon*afstro_sum_month/100,
          
-         leach_rf_full=pred_rf_full*afstro_sum_month/100,
-         leach_rf_full_ind=pred_rf_full_ind*afstro_sum_month/100,
+    leach_rf_full=pred_rf_full*afstro_sum_month/100,
+    leach_rf_full_ind=pred_rf_full_ind*afstro_sum_month/100,
+    leach_rf_sim=pred_rf_sim*afstro_sum_month/100,
+    leach_rf_sim_ind=pred_rf_sim_ind*afstro_sum_month/100,
+    leach_rf_sim_max=pred_rf_sim_ind_max*afstro_sum_month/100,
          
-         leach_rf_sim=pred_rf_sim*afstro_sum_month/100,
-         leach_rf_sim_ind=pred_rf_sim_ind*afstro_sum_month/100,
+    leach_gbm_sim_ind= pred_gbm_sim_ind*afstro_sum_month/100,
+    leach_xbt_sim_ind =pred_xbt_sim_ind*afstro_sum_month/100,
+    leach_xbt_sim_ind_max =pred_xbt_sim_ind_max*afstro_sum_month/100,
+    
          
-         leach_lin=pred_lin*afstro_sum_month/100,
-         leach_lin_ind=pred_lin_ind*afstro_sum_month/100)
+    leach_lin=pred_lin*afstro_sum_month/100,
+    leach_lin_ind=pred_lin_ind*afstro_sum_month/100)
 
 testRes  <-  df_gen_pred |> select(starts_with("leach_")) |> corrplot::cor.mtest(conf.level = 0.95)
 
 df_gen_pred |> select(starts_with("leach_")) |> cor() |>
-  corrplot::corrplot( p.mat = testRes$p, method = 'circle', 
+  corrplot::corrplot( method = 'circle', 
                       type = 'lower', addCoef.col ='White',
-           insig='blank',tl.col = 'black',
-           order = 'AOE', diag = FALSE)
+                      #insig='blank', 
+                      tl.col = 'black',
+                      #col=gray.colors(100),
+                      order = 'AOE', diag = FALSE)
+
+saveRDS(df_gen, "df_gen0610.RDS")
+
+df_gen_pred |>
+  mutate(clay_plot = fct_recode(
+    clay_cat,
+    low = "low",
+    'midle-high' = "middle",
+    'midle-high' = "high"
+  )) |>
+  filter(Winter_nles5 == c("1", "4")) |>
+  mutate(Winter_nles5=dplyr::recode_factor(Winter_nles5,
+                `1`="Winter cereal", 
+                `4`="Cover crop" ) )|> 
+  #filter(Main_nles5==1 & Winter_nles5 == 4 & harvest_year== c(1998,1999,2008))|>
+  #select(month, rf_pred, meancon,jbnr,harvest_year,clay_cat) |>
+  pivot_longer(
+    cols = c(
+      leach_a_obs,
+      leach_xbt_sim_ind,
+      leach_rf_full_ind,
+      leach_lin_ind
+    ),
+    #starts_with("leach_"),
+    values_to = "Nleaching",
+    names_to = "Measurement"
+  ) |>
+  ggplot(aes(
+    x = month,
+    y = Nleaching,
+    fill = Measurement,
+    col = Measurement
+  )) +
+   geom_smooth(alpha = 0.4, se=FALSE #aes(linetype = Measurement)
+               ) +
+  geom_boxplot(alpha = .1, aes(group = interaction(Measurement, month), 
+                               fill = Measurement)) +
+  geom_point(alpha = 0.2, size = 0.1) +
+  facet_grid(Winter_nles5 ~ clay_plot) +
+  scale_x_continuous(breaks = seq(1, 12, 1)) +
+  scale_y_continuous(limits = c(0, 30)) +
   
-saveRDS(df_gen, "df_gen1809.RDS")
-
-df_gen_pred |> 
-  mutate(clay_plot=fct_recode(clay_cat,low="low", 'midle-high'="middle", 'midle-high'="high")) |> 
-  filter(Winter_nles5==c("1","3","5","9")) |> 
-  #filter(Main_nles5==1 & Winter_nles5 == 4 & harvest_year== c(1998,1999,2008))|> 
-  #select(month, rf_pred, meancon,jbnr,harvest_year,clay_cat) |> 
-  pivot_longer(cols=c(leach_obs,leach_rf_full, leach_rf_full_ind,leach_lin_ind),#starts_with("leach_"), 
-               values_to = "Nleaching", names_to = "Measurement") |> 
-  ggplot(aes(x=month,y=Nleaching,fill=Measurement, col=Measurement))+
-  geom_boxplot(alpha=.1, aes(group=interaction(Measurement,month), fill=Measurement
-  ) )+
- # scale_color_manual(values=c("#999999", "#E69F00", "#9467bd"))+
- # scale_fill_manual(values=c("#999999", "#E69F00", "#9467bd"))+
-  geom_smooth(alpha=0.1, aes(linetype=Measurement))+
-  geom_point(alpha=0.2, size=0.1)+
-  facet_grid(Winter_nles5~clay_plot)+
-  scale_x_continuous(breaks = seq(1,12,1))+
-  scale_y_continuous(limits = c(0,30))+
-  theme(legend.position = "bottom", panel.background = NULL)+
-  guides(linetype=FALSE,fill=FALSE, col=guide_legend("Estimation:"))+
-  ylab("N leaching (Kg N/ha)")#+
-  #theme_bw()
+  guides(linetype = FALSE,
+         fill = FALSE,
+         col = guide_legend("Estimation:")) +
+  ylab("N leaching (Kg N/ha)")+
+  ggthemes::theme_hc()+
+  scale_color_gdocs()+
+  scale_fill_gdocs()+
+  theme(legend.position = "bottom", panel.background = NULL) 
+#theme_bw()
 
 
 
-df_gen_pred |> 
-  #mutate(clay_plot=fct_recode(clay_cat,low="low", 'midle-high'="middle", 'midle-high'="high")) |> 
-  #filter(Winter_nles5==c("1","3","5","9")) |> 
-  #filter(Main_nles5==1 & Winter_nles5 == 4 & harvest_year== c(1998,1999,2008))|> 
-  #select(month, rf_pred, meancon,jbnr,harvest_year,clay_cat) |> 
-  pivot_longer(cols=c(leach_obs,leach_rf_full, leach_rf_full_ind,leach_lin_ind),#starts_with("leach_"), 
-               values_to = "Nleaching", names_to = "Measurement") |> 
-  ggplot(aes(x=log(Nleaching) , fill=Measurement, col=Measurement))+
-  #geom_density(stat="density", position="identity", alpha=0.2)+
+log_den<- df_gen_pred |>
+  pivot_longer(
+    cols = c(
+      leach_a_obs,
+      leach_xbt_sim_ind,
+      leach_rf_full_ind,
+      leach_lin_ind
+    ),
+    values_to = "Nleaching",
+    names_to = "Measurement"
+  ) |>
+  ggplot(aes(
+    x = log(Nleaching) ,
+    fill = Measurement,
+    col = Measurement
+  )) +
+  geom_density(stat="density", position="identity", alpha=0.1)+
+  #geom_step(aes(y = ..y..), stat = "ecdf") +
+  #scale_x_continuous(breaks = c(-2.5, 5.5)) +
+  #scale_x_continuous(limits = c(0,75))+
+  theme_hc() +
+  theme(legend.position = "bottom", 
+        panel.background = NULL#,
+        #panel.border =NULL
+        ) +
+  guides(linetype = FALSE,
+         fill = FALSE,
+         col = guide_legend("Estimation:")) +
+  #ylab("Percentile")+
+  ylab("Frequency")+
+  scale_color_gdocs()+
+  scale_fill_gdocs()
+
+ggarrange(log_ecdf, ecdf, log_den, den, ncol=2, nrow=2, #, 
+          common.legend = T,  legend="bottom")
+
+
+# Lookup table ----
+
+#https://climateknowledgeportal.worldbank.org/country/denmark/climate-data-historical
+
+cont <- 
+  df_gen_simp_ind_max |> 
+  select(!c(month, meancon)) |> 
+  select_if(is.numeric) |>
+  scale(center = FALSE) |> as.data.frame() 
+
+cont |>  
+  pivot_longer(
+    cols =colnames(cont),
+    values_to = "value",
+    names_to = "variable"
+  ) |>
+  ggplot(aes(
+    x = value ,
+    fill = variable,
+    col = variable
+  )) +
+  geom_step(aes(y = ..y..), stat = "ecdf") +
+  #geom_density(stat="density", position="identity", alpha=0.1)+
+  theme_hc() +
+  theme(legend.position = "bottom", 
+        panel.background = NULL#,
+        #panel.border =NULL
+  ) +
+  guides(linetype = FALSE,
+         fill = FALSE,
+         col = guide_legend("variable")) +
+  ylab("Percentile")+
+  #ylab("Frequency")+
+  scale_color_gdocs()+
+  scale_fill_gdocs()
   
-  geom_step(aes(y=..y..),stat="ecdf")+
-  #geom_boxplot(alpha=.1, aes(group=interaction(Measurement,month), fill=Measurement
- # ) )+
-  # scale_color_manual(values=c("#999999", "#E69F00", "#9467bd"))+
-  # scale_fill_manual(values=c("#999999", "#E69F00", "#9467bd"))+
-  #geom_smooth(alpha=0.1, aes(linetype=Measurement))+
-  #geom_point(alpha=0.2, size=0.1)+
-  #facet_grid(Winter_nles5~clay_plot)+
-  scale_x_continuous(breaks = c(-10,5))+
-  #scale_y_continuous(limits = c(0,30))+
-  theme_bw()+
-  theme(legend.position = "bottom", panel.background = NULL)+
-  guides(linetype=FALSE,fill=FALSE, col=guide_legend("Estimation:"))+
- ylab("Percentile")
-  # ylab("Frequency")
+df_gen_simp_ind_max |>  
+  ggplot(aes(x = N_mineral_spring )) +
+  #geom_density(stat="density", position="identity", alpha=0.1)+
+  geom_step(aes(y = ..y..), stat = "ecdf")+
+  theme_hc()
+
+quantile(df_gen_simp_ind_max$N_mineral_spring, seq(0.1,1,0.1))
+
+quantile(df_gen_simp_ind_max$AirTemp_ave_month, seq(0.1,1,0.1))
+
+quantile(df_gen_simp_ind_max$Precip_cumsumhy, seq(0.1,1,0.1))
+
+fitt_xgb_max <- readRDS("fitt_xgb_max.RDS")
+
+lookup_01_xbt <- expand.grid(month=seq(1,12,1),
+                         Main_nles5=levels(df_gen_simp_ind_max$Main_nles5),
+                         Winter_nles5=levels(df_gen_simp_ind_max$Winter_nles5),
+                         clay_cat=levels(df_gen_simp_ind_max$clay_cat),
+                         AirTemp_ave_month=seq(-1,25,1),
+                         Precip_cumsumhy=c(seq(10,550,20), seq(600,1400,50)),
+                         N_mineral_spring=seq(0,320,20)
+                         )
+
+lookup_01_xbt$pred <- predict(fitt_xgb_max,lookup_01_xbt)
+
+lookup_01_xbt$pred <- exp(lookup_01_xbt$pred)
+
+write.table(lookup_01_xbt,"lookup_01_xbt.txt", sep="\t")
+             
